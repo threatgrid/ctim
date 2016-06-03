4c6d0f6d261df946a12ca77e42402556304912c4
Paula Gearon <pgearon@cisco.com>
2016-06-02 22:08:44 -0400

    Added StoredVerdict to verdicts

--- src/ctim/schemas/verdict.clj
+++ src/ctim/schemas/verdict.clj
@@ -4,6 +4,9 @@
             [schema.core :as s]
             [schema-tools.core :as st]))
 
+(s/defschema Type
+  (s/enum "verdict"))
+
 (s/defschema Verdict
   "A Verdict is chosen from all of the Judgements on that Observable
 which have not yet expired.  The highest priority Judgement becomes
@@ -11,7 +14,15 @@ the active verdict.  If there is more than one Judgement with that
 priority, then Clean disposition has priority over all others, then
 Malicious disposition, and so on down to Unknown.
 "
-  {:type (s/enum "verdict")
+  {:type Type
    :disposition c/DispositionNumber
    (s/optional-key :judgement_id) rel/JudgementReference
    (s/optional-key :disposition_name) c/DispositionName})
+
+(s/defschema StoredVerdict
+  "A Verdict as stored in the data store"
+  (st/merge Verdict
+            {:id s/Str
+             :type Type
+             :owner s/Str
+             :created c/Time}))
