f1be9e4b9c8144eb4602aa8e43b41f4169095250
Stephen Sloan <steve@stevesloan.com>
2016-08-24 13:49:17 -0700

    Using clj-momo 0.2.1

--- project.clj
+++ project.clj
@@ -21,7 +21,7 @@
                  [com.andrewmcveigh/cljs-time "0.5.0-alpha1"]
                  [clj-time "0.12.0"]
                  ;; shared libs
-                 [threatgrid/clj-momo "0.2.0"]]
+                 [threatgrid/clj-momo "0.2.1"]]
 
   :uberjar-name "ctim.jar"
   :resource-paths ["doc"]
