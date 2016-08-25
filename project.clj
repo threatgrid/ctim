c8c7ff3ee4cce013763625070de3f22beb5c3baa
Stephen Sloan <steve@stevesloan.com>
2016-08-25 12:48:09 -0700

    Patch release wich updates the schema version to 1.1.3

--- project.clj
+++ project.clj
@@ -1,15 +1,18 @@
-(defproject threatgrid/ctim "0.1.6"
+(def schema-tools-version "0.9.0")
+(def schema-version "1.1.3")
+
+(defproject threatgrid/ctim "0.1.6-1"
   :description "Cisco Threat Intelligence Model"
   :url "http://github.com/threatbrain/ctim"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  ;; what compojure-api 1.0.0 wants
-                 [prismatic/schema "1.1.2"]
+                 [prismatic/schema ~schema-version]
                  ;; for schema descriptions
                  [metosin/ring-swagger "0.22.9"]
                  ;; for merge and such
-                 [metosin/schema-tools "0.9.0"]
+                 [metosin/schema-tools ~schema-tools-version]
                  ;; for generators
                  [org.clojure/test.check "0.9.0"]
                  [com.gfredericks/test.chuck "0.2.6"]
