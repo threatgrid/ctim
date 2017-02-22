70790d6bc3f2f7c33b3e133aaa23e3db0f0b1760
Stephen Sloan <steve@stevesloan.com>
2017-02-22 15:36:51 -0800

    Bumped version

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (def schema-tools-version "0.9.0")
 (def schema-version "1.1.3")
 
-(defproject threatgrid/ctim "0.4.5"
+(defproject threatgrid/ctim "0.4.6-SNAPSHOT"
   :description "Cisco Threat Intelligence Model"
   :url "http://github.com/threatbrain/ctim"
   :license {:name "Eclipse Public License"
