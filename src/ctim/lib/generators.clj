043cdf0b9072687a1a88d7dd0765d8037ee40c01
Stephen Sloan <steve@stevesloan.com>
2017-09-07 13:25:57 -0700

    Fixed slow generators issue (filling strings with \0 instead of random chars)

--- src/ctim/lib/generators.clj
+++ src/ctim/lib/generators.clj
@@ -3,5 +3,8 @@
    [clojure.test.check.generators :as gen]))
 
 (defn string-max-len [max-len]
-  (gen/fmap #(apply str %)
-            (gen/vector gen/char-ascii 0 max-len)))
+  (gen/fmap (fn [size]
+              (apply str
+                     (repeatedly size (constantly \0))))
+            (gen/resize max-len
+                        gen/pos-int)))
