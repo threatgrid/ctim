1c847c807c20fea7ab4e43e4d1fa03b629ce7dec
Ambrose Bonnaire-Sergeant <ambonnai@cisco.com>
2020-02-12 13:32:16 -0500

    remove comment around ctim.generators.events

--- src/ctim/generators/events.clj
+++ src/ctim/generators/events.clj
@@ -1,4 +1,3 @@
-(comment
 (ns ctim.generators.events
   (:require [clojure.test.check.generators :as gen :include-macros true]
             [ctim.events.schemas :as es]
@@ -21,4 +20,3 @@
              (gi/gen-url-id-of-type :event)
              ge/gen-any-example-entity
              gen-event-type)))
-)
