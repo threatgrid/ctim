390a89122e080199f9138669e7e41ee674bd3b57
Ag Ibragimov <agzam.ibragimov@gmail.com>
2020-07-29 11:31:41 -0700

    puts AssetMapping into separate namespaces
    Addressing PR comments:
    
    https://github.com/threatgrid/ctim/pull/292#pullrequestreview-455874516
    https://github.com/threatgrid/ctim/pull/292#pullrequestreview-457204632

--- src/ctim/document.clj
+++ src/ctim/document.clj
@@ -5,6 +5,7 @@
             [ctim.schemas
              [actor :as a]
              [asset :as asset]
+             [asset-mapping :as asset-mapping]
              [attack-pattern :as attack]
              [bundle :as bu]
              [campaign :as c]
@@ -37,7 +38,7 @@
            ["json/actor.json" a/Actor ->json]
            ["json/asset.json" asset/Asset ->json]
            ["structures/asset.md" asset/Asset ->markdown]
-           ["json/asset_mapping.json" asset/AssetMapping ->json]
+           ["json/asset_mapping.json" asset-mapping/AssetMapping ->json]
            ["structures/attack_pattern.md" attack/AttackPattern ->markdown]
            ["json/attack_pattern.json" attack/AttackPattern ->json]
            ["structures/bundle.md" bu/Bundle ->markdown]
