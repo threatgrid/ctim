35f89e228244a61222f15cf15735ff938a8d4376
Ag Ibragimov <agzam.ibragimov@gmail.com>
2020-08-27 17:31:41 -0700

    inc version

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (def schema-tools-version "0.9.1")
 (def schema-version "1.1.7")
 
-(defproject threatgrid/ctim "1.0.20-SNAPSHOT"
+(defproject threatgrid/ctim "1.0.21-SNAPSHOT"
   :description "Cisco Threat Intelligence Model"
   :url "http://github.com/threatgrid/ctim"
   :license {:name "Eclipse Public License"
