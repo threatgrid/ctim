91809eb29e92ac62d6d2cd94598ad9e31ad50623
Ambrose Bonnaire-Sergeant <ambrose@ambrosebs.com>
2022-02-22 12:25:58 -0500

    Move to GitHub actions (#364)

--- project.clj
+++ project.clj
@@ -10,13 +10,12 @@
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [prismatic/schema "1.1.12"]
                  [com.google.protobuf/protobuf-java "3.7.1"] ;clj-momo > org.clojure/clojurescript
-                 [instaparse "1.4.8"] ;clj-momo > com.gfredericks/test.chuck
                  [threatgrid/clj-momo "0.3.5"]
                  [org.mozilla/rhino "1.7.7.1"] ;threatgrid/flanders > kovacnica/clojure.network.ip
                  [threatgrid/flanders "0.1.23"]
                  [metosin/ring-swagger "0.26.2"]
-                 [org.clojure/test.check "0.10.0"]
-                 [com.gfredericks/test.chuck "0.2.10"]
+                 [org.clojure/test.check "1.1.1"]
+                 [com.gfredericks/test.chuck "0.2.13"]
                  [prismatic/schema-generators "0.1.3"]
                  [kovacnica/clojure.network.ip "0.1.3"]]
 
