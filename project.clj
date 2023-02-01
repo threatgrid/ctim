5870320a8aacc21d47308b572da618ab54a2266d
Guillaume Buisson <guillaume@gbuisson.com>
2023-02-01 14:37:56 +0100

    Bump to 1.2.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject threatgrid/ctim "1.2.2-SNAPSHOT"
+(defproject threatgrid/ctim "1.2.2"
   :description "Cisco Threat Intelligence Model"
   :url "http://github.com/threatgrid/ctim"
   :license {:name "Eclipse Public License"
--- src/ctim/schemas/common.cljc
+++ src/ctim/schemas/common.cljc
@@ -20,7 +20,7 @@
             [flanders.predicates :as fp]
             [clojure.string :as str]))
 
-(def ctim-schema-version "1.2.1")
+(def ctim-schema-version "1.2.2")
 
 (def-eq CTIMSchemaVersion ctim-schema-version)
 
