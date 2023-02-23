5f381c3708ab6a199d6a8ee298d01fe293fbf3c7
Ambrose Bonnaire-Sergeant <ambonnai@cisco.com>
2023-02-23 11:02:47 -0500

    prep 1.3.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject threatgrid/ctim "1.2.3-SNAPSHOT"
+(defproject threatgrid/ctim "1.3.0-SNAPSHOT"
   :description "Cisco Threat Intelligence Model"
   :url "http://github.com/threatgrid/ctim"
   :license {:name "Eclipse Public License"
