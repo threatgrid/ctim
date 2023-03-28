5077dbd3616a0e890f9e6db3e0908bf50d986fcf
Matthieu Sprunck (msprunck) <matthieu@sprunck.com>
2023-03-28 17:38:24 +0200

    Bump to 1.3.8-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject threatgrid/ctim "1.3.7"
+(defproject threatgrid/ctim "1.3.8-SNAPSHOT"
   :description "Cisco Threat Intelligence Model"
   :url "http://github.com/threatgrid/ctim"
   :license {:name "Eclipse Public License"
