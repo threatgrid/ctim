741682ebde8029478a0c442c0d49126922d35482
Guillaume ERETEO <gereteo@cisco.com>
2024-11-13 13:52:19 +0100

    version 1.3.20

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject threatgrid/ctim "1.3.20-SNAPSHOT"
+(defproject threatgrid/ctim "1.3.20"
   :description "Cisco Threat Intelligence Model"
   :url "http://github.com/threatgrid/ctim"
   :license {:name "Eclipse Public License"
