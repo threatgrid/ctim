4760b6ca480f225016e1c73b867adde42663c565
Ambrose Bonnaire-Sergeant <ambonnai@cisco.com>
2024-11-26 13:35:47 -0600

    Version 1.3.22-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject threatgrid/ctim "1.3.21"
+(defproject threatgrid/ctim "1.3.22-SNAPSHOT"
   :description "Cisco Threat Intelligence Model"
   :url "http://github.com/threatgrid/ctim"
   :license {:name "Eclipse Public License"
