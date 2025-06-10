54bac2eb9e56689b3fc670db85bdb504bee3e928
Ambrose Bonnaire-Sergeant <ambonnai@cisco.com>
2025-06-10 17:06:12 -0500

    wip

--- src/ctim/version.clj
+++ src/ctim/version.clj
@@ -1,5 +1,6 @@
 (ns ctim.version
-  (:require [clojure.java.io :as io]))
+  (:require [clojure.java.io :as io]
+            [clojure.string :as str]))
 
 (defmacro ctim-version
   "This is a macro to support cljs compile-time inlining of the version string
@@ -10,4 +11,11 @@
                        (doto (java.util.Properties.)
                          (.load pom-properties-reader))))]
     (or (get properties "version")
+        (System/getProperty "ctim.version-override")
+        (when (= "true" (System/getProperty "ctim.dev"))
+          (-> "VERSION_TEMPLATE"
+              slurp
+              str/trim
+              (str/replace "GENERATED_VERSION" "999999")
+              (str "-SNAPSHOT")))
         (throw (ex-info "Unable to resolve ctim version" {})))))
