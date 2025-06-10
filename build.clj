09f0639bc4ced3bdbc303610d5b3b544ce071e74
Ambrose Bonnaire-Sergeant <ambonnai@cisco.com>
2025-06-10 16:27:52 -0500

    wip

--- build.clj
+++ build.clj
@@ -1,6 +1,11 @@
 (ns build
-  (:require [clojure.string :as str]
-            [clojure.tools.build.api :as b])
+  (:require [clojure.edn :as edn]
+            [clojure.string :as str]
+            [clojure.tools.build.api :as b]
+            [clojure.pprint :as pp]
+            [clojure.java.io :as io]
+            [clojure.walk :as walk]
+            [clj-commons.digest :as digest])
   (:import (java.util.regex Pattern)))
 
 (def lib 'threatgrid/ctim)
@@ -18,25 +23,29 @@
                 :lib lib
                 :version version
                 :basis basis
-                :scm {:connection "scm:git:git://github.com/threatgrid/ctim.git"
+                :scm {:url "https://github.com/threatgrid/ctim"
+                      :connection "scm:git:git://github.com/threatgrid/ctim.git"
                       :developerConnection "scm:git:git@github.com:threatgrid/ctim.git"
                       :tag (if (str/ends-with? version "-SNAPSHOT")
                              (b/git-process {:git-args "rev-parse HEAD"})
                              ;; we commit the checksum of the jar to the repo ahead of time to ensure it is reproducible.
                              ;; so it cannot contain the release SHA.
                              ;; TODO include command to reproduce jar
+                             ;; TODO push sha to maven-metadata.xml
                              version)}
+                ;;TODO copy from basis
                 :src-dirs ["src"]})
-  (b/copy-dir {:src-dirs ["src" "resources"]
+  (b/copy-dir {;;TODO copy from basis
+               :src-dirs ["src" "doc"]
                :target-dir class-dir})
-  (b/jar {:class-dir class-dir
-          :jar-file (format "target/%s-%s.jar" (name lib) version)})
-  params)
+  (let [jar-file (format "target/%s-%s.jar" (name lib) version)]
+    (b/jar {:class-dir class-dir
+            :jar-file (format "target/%s-%s.jar" (name lib) version)})
+    (assoc params :jar-file jar-file)))
 
 (defn tag-release [{:keys [version] :as params}]
   {:pre [version]}
   (b/git-process {:git-args (format "tag %s" version)})
-  (b/git-process {:git-args (format "push origin tag %s" version)})
   params)
 
 (defn deploy-branch? [params]
@@ -77,11 +86,11 @@
 
 (defn next-version []
   (b/git-process {:git-args (format "fetch tags")})
-  (str (instantiate-version-template (or (some-> (prev-release-in-series)
-                                                 generated-version
-                                                 inc')
-                                         0))
-       (when-not release "-SNAPSHOT")))
+  (let [next-generated-version (or (some-> (prev-release-in-series)
+                                           generated-version
+                                           inc')
+                                   0)]
+    (instantiate-version-template next-generated-version)))
 
 (comment
   (next-version)
@@ -99,8 +108,52 @@
       jar))
 
 (defn build-release [params]
+  ;;TODO build docs
   (-> params
       (assoc :release true)
       infer-version
-      jar)
+      jar))
+
+(defn- serialize [m]
+  (binding [*print-namespace-maps* false
+            *print-level* nil
+            *print-length* nil]
+    (with-out-str
+      (pp/pprint
+        (walk/postwalk
+          (fn [m]
+            (cond
+              (map? m) (into (sorted-map) m)
+              (set? m) (into (sorted-set) m)
+              :else m))
+          m)))))
+
+(defn checksums [file]
+  {;; clojars uses these
+   :md5 (digest/md5 file)
+   :sha-1 (digest/sha-1 file)
+   ;; something stronger
+   :sha3-512 (digest/sha3-512 file)})
+
+(defn update-reproducible-releases [{:keys [version jar-file] :as params}]
+  (let [cs (checksums (io/file jar-file))
+        prev-releases (-> "reproducible-releases.edn"
+                           io/file
+                           slurp
+                           edn/read-string)
+        new-releases (assoc prev-releases version {:git-tag version
+                                                   :parent-commit (b/git-process {:git-args "rev-parse master"})
+                                                   :reproduction {:command (format "clojure -T:build build-release :version %s" version)
+                                                                  :artifact->checksums {jar-file cs}}})]
+    (spit "reproducible-releases.edn" (serialize new-releases))))
+
+(defn schedule-release [params]
+  (let [{:keys [version] :as params} (-> params
+                                         build-release
+                                         update-reproducible-releases)]
+    ;; TODO gen docs
+    (b/git-process {:git-args "add ."})
+    (b/git-process {:git-args (format "commit -m '[:ctim-release %s]'" version)})))
+
+(defn perform-release [params]
   (tag-release params))
