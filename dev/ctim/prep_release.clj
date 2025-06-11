85fefa79d9d02c20765730258e12e123d15cb7cf
Ambrose Bonnaire-Sergeant <ambonnai@cisco.com>
2025-06-11 14:07:24 -0500

    wip

--- dev/ctim/prep_release.clj
+++ dev/ctim/prep_release.clj
@@ -2,12 +2,28 @@
   (:require [ctim.version :refer [-ctim-version]]
             [clojure.java.shell :as sh]))
 
+(defn- sh [& args]
+  (let [{:keys [exit out err]} (apply sh/sh args)]
+    (println out)
+    (println err)
+    (assert (zero? exit))))
+
+(defn assert-clean []
+  (let [{:keys [out exit]} (sh/sh "git" "status" "--short")]
+    (assert (zero? exit))
+    (println out)
+    (when (seq out)
+      (println "Working directory is not clean, exiting")
+      (System/exit 1))))
+
 (defn -main []
+  (assert-clean)
   (loop [prev-version nil
          version (-ctim-version)]
     (if (= prev-version version)
-      (do (sh/sh ["git" "add" "resources/ctim/version.edn"])
-          (sh/sh ["git" "commit" "-m" (format "[release ctim] " version)]))
+      (do (sh ["git" "add" "resources/ctim/version.edn"])
+          (sh ["git" "commit" "-m" (format "[release ctim] " version)])
+          (System/exit 0))
       (do (println (str "Preparing to release " (-ctim-version)))
           (println (str "If this is ok, press enter. Otherwise please update `resources/ctim/version.edn` before pressing enter."))
           (read-line)
