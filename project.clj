fba98f02d89e01377d52437042ac98ce4f82a3ea
Guillaume Buisson <guillaume@gbuisson.com>
2025-10-02 16:15:54 +0200

    Version 1.3.28-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject threatgrid/ctim "1.3.27"
+(defproject threatgrid/ctim "1.3.28-SNAPSHOT"
   :description "Cisco Threat Intelligence Model"
   :url "http://github.com/threatgrid/ctim"
   :license {:name "Eclipse Public License"
